8528178949b08cc95f5168bd9f8023f541fa56ec
Alexandre de Souza <alexandre@aledsz.com.br>
2019-12-04 14:45:15 -0300

    Update app version to use from module attribute

--- mix.exs
+++ mix.exs
@@ -1,10 +1,11 @@
 defmodule TypoKiller.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
   def project do
     [
       app: :typo_killer,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
