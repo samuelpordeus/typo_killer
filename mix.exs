7b0b1bd2502a31fd1c5233ac8a769e78bfbb6882
Alexandre de Souza <alexandre@aledsz.com.br>
2019-12-04 14:21:50 -0300

    Update elixir version on mix file

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TypoKiller.MixProject do
     [
       app: :typo_killer,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
