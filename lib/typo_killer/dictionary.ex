c5835ada4d1d7463642376eae4a5e8a51830765a
Alexandre de Souza <alexandre@aledsz.com.br>
2019-12-04 15:12:22 -0300

    Remove unused functions

--- lib/typo_killer/dictionary.ex
+++ lib/typo_killer/dictionary.ex
@@ -21,13 +21,4 @@ defmodule TypoKiller.Dictionary do
 
     {words, dictionary}
   end
-
-  def aux_dictionary, do: @ignored_words_list
-
-  def remove_ignored_words(words) do
-    words
-    |> MapSet.new()
-    |> MapSet.difference(@ignored_words_mapset)
-    |> MapSet.to_list()
-  end
 end
