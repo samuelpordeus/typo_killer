c8aa4bf2da024452a080104ff67176c470e09ada
Alexandre de Souza <alexandre@aledsz.com.br>
2019-12-04 15:15:51 -0300

    Add docs and specs to FileParser module

--- lib/typo_killer/file_parser.ex
+++ lib/typo_killer/file_parser.ex
@@ -1,4 +1,16 @@
 defmodule TypoKiller.FileParser do
+  @moduledoc """
+  Parse all files and concat paths to folder to generate a complete list of files ready to be read
+  """
+
+  @doc """
+  Find files inside path.
+
+  - When path is a file, it returns itself inside a list
+  - When path is a directory, scan the entire folder and subfolders for files
+  - When path is something different, it returns an empty list
+  """
+  @spec find_files_on_folder(path :: String.t) :: list(String.t) | []
   def find_files_on_folder(path \\ ".") do
     cond do
       File.regular?(path) ->
