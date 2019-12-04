7f924c333f3cd5067a697c3d65e5dd623f101f43
Alexandre de Souza <alexandre@aledsz.com.br>
2019-12-04 14:22:38 -0300

    Add dialyzer args on mix project file

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule TypoKiller.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_add_deps: :transitive,
+        plt_add_apps: [:mix],
+        remove_defaults: [:unknown]
+      ]
     ]
   end
 
